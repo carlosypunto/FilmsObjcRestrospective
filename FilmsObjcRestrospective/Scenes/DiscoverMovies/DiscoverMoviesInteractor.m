38b8323efe0e815f16fc875a5cd916d7b1efbc1c
Carlos García <carlosypunto@gmail.com>
2019-02-11 08:30:13 +0100

    retrieve DiscoverMoviesInteractor data

--- FilmsObjcRestrospective/Scenes/DiscoverMovies/DiscoverMoviesInteractor.m
+++ FilmsObjcRestrospective/Scenes/DiscoverMovies/DiscoverMoviesInteractor.m
@@ -7,13 +7,52 @@
 //
  
 #import "DiscoverMoviesInteractor.h"
+#import "APIClient.h"
+#import "DefaultAPIClient.h"
+
+@interface DiscoverMoviesInteractor ()
+
+@property (nonatomic, assign) int currentPage;
+@property (nonatomic, assign) int totalPages;
+@property (nonatomic, assign, getter=isSearching) BOOL searching;
+@property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong, nullable) id <APIClient> apiClient;
+
+@end
 
 @implementation DiscoverMoviesInteractor
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.apiClient = [[DefaultAPIClient alloc] init];
+        self.movies = @[];
+        self.searching = NO;
+        self.currentPage = 1;
+    }
+    return self;
+}
+
 
 #pragma mark - InteractorProtocol
 
 - (void)makeRequest {
+    [self setSearching: YES];
     
+    [self.apiClient getDiscoverMoviePage:_currentPage
+                                 success:^(DiscoverResponse *response) {
+                                     [self setTotalPages:[response.totalPages intValue]];
+                                     
+                                     NSArray *newMovies = [self.movies arrayByAddingObjectsFromArray:[response results]];
+                                     self.movies = newMovies;
+                                     
+                                     [self.output updateMovies:self.movies];
+                                     [self setSearching: NO];
+                                     self.currentPage++;
+                                 }
+                                 failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                                     NSLog(@"%@", error);
+                                 }];
 }
 
 
