29482e18dfa6661dc385bc4e865072fab0e0550e
Carlos García <carlosypunto@gmail.com>
2019-02-12 00:02:05 +0100

    add default project colors category

---
+++ FilmsObjcRestrospective/Categories/UIColor+projectColors.h
@@ -0,0 +1,19 @@
+//
+//  UIColor+projectColors.h
+//  FilmsObjcRestrospective
+//
+//  Created by Carlos on 11/02/2019.
+//  Copyright © 2019 Carlos García Nieto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIColor (ProjectColors)
+
++ (UIColor *)lightGray;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FilmsObjcRestrospective/Categories/UIColor+projectColors.m
@@ -0,0 +1,17 @@
+//
+//  UIColor+projectColors.m
+//  FilmsObjcRestrospective
+//
+//  Created by Carlos on 11/02/2019.
+//  Copyright © 2019 Carlos García Nieto. All rights reserved.
+//
+
+#import "UIColor+projectColors.h"
+
+@implementation UIColor (ProjectColors)
+
++ (UIColor *)lightGray {
+    return [UIColor colorWithRed: 240.0/255.0 green: 240.0/255.0 blue: 240.0/255.0 alpha: 1.0];
+}
+
+@end
